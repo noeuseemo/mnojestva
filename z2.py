fa6e5f369209f1a0ef85e1f84eca67a202428c1b
kirill <143942709+noeuseemo@users.noreply.github.com>
2023-10-26 11:03:53 +0700

    first commit

--- z2.py
+++ z2.py
@@ -1,5 +1,4 @@
-a = input('первое число-  ')
-b = input('второе число-  ')
-
-n = a + b
-print(len(n))
+lst1= [2,1,4,6,1,8,5,3]
+lst2= [4,1,6,4,7,1,7,8]
+print (set(lst1) & set(lst2))
+print (len(set(lst1) & set (lst2)))
